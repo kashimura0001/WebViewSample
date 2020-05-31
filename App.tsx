ce7456bd4d85d3b80d3182b21a1612eef4748375
keisukekashimura <kashimura@refcome.com>
2020-05-31 11:53:55 +0900

    fix webview

--- App.tsx
+++ App.tsx
@@ -1,47 +1,54 @@
-import React, {useEffect, useRef, useState} from 'react';
-import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useRef, useState} from 'react';
+import {SafeAreaView} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useIsFocused, NavigationContainer} from '@react-navigation/native';
 
+// webview screen
+const WebViewScreen = ({route, navigation}: any) => {
+  const webViewRef = useRef<WebView>(null);
+  const isFocused = useIsFocused();
+  const [isFirstLoading, setIsFirstLoading] = useState(true);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <WebView
+        source={{uri: route?.params?.url || 'https://www.google.co.jp/'}}
+        ref={webViewRef}
+        onLoadStart={({nativeEvent}) => {
+          if (isFirstLoading || !isFocused) {
+            return;
+          }
+          navigation.push('WEB_VIEW', {url: nativeEvent.url});
+        }}
+        onLoad={() => {
+          setIsFirstLoading(false);
+          webViewRef?.current?.goBack();
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+// navigation
 const Stack = createStackNavigator();
-
 const Navigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name={"WEB_VIEW"} component={WebViewScreen} />
+        <Stack.Screen
+          name={'WEB_VIEW'}
+          component={WebViewScreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const WebViewScreen = ({route, navigation}: any) => {
-  const webViewRef = useRef<WebView>(null);
-  const [pageLoaded, setPageLoaded] = useState(false);
-
-  return (
-    <View style={{flex: 1}}>
-      <WebView
-        source={{ uri: route?.params?.url || "https://kasssssy.hatenablog.com/" }}
-        ref={webViewRef}
-        onLoadStart={({ nativeEvent }) => {
-          if(pageLoaded) {
-            webViewRef?.current?.stopLoading();
-            navigation.push('WEB_VIEW', { url: nativeEvent.url })
-          }
-          setPageLoaded(true)
-        }}
-      />
-    </View>
-  );
-};
-
+// root component
 const App = () => {
-  return (
-    <Navigator />
-  );
+  return <Navigator />;
 };
-
 
 export default App;
